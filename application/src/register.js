804d4a0bb128c930cb37b33321c1191097792c22
Elijah <Superil2q@yandex.ru>
2020-02-22 22:53:51 +0300

    register page

--- application/src/register.js
+++ application/src/register.js
@@ -7,6 +7,81 @@ const help = require('./utils/help');
 class Register extends React.Component {
     constructor(prop) {
         super(prop);
+        this.state = {
+            class : {
+                name : '',
+                user : '',
+                pw1 : '',
+                pw2 : ''
+            },
+
+            info : 'green-color',
+            msg : ''
+        };
+    }
+
+    register() {
+
+        let data = help.validate(document,['name','login','pw1','pw2']);
+        
+        if(data.errors.length !== 0)
+        {
+            let newStyle = {
+                name : '',
+                login : '',
+                pw1 : '',
+                pw2 : ''
+            };
+            for(let i = 0; i < data.errors.length; ++i)
+            {
+                newStyle[data.errors[i]] = 'mistake';
+            }
+
+            this.setState({
+                class : newStyle
+            })
+        }
+        else
+        {
+            let pw1 = help.val(document,'pw1');
+            let pw2 = help.val(document,'pw2');
+
+            console.log(pw1);
+
+            if(pw1 != pw2)
+            {
+                let styles = this.state.class;
+                styles.pw1 = 'mistake';
+                styles.pw2 = 'mistake';
+                this.setState({ class : styles});
+            }
+            else
+            {
+                axios.post('/register',{
+                    name : data.name,
+                    username : data.login,
+                    password : data.pw1
+                }).then(() => {
+                    this.setState({
+                        info : 'green-color',
+                        msg : 'Успешно!'
+                    })
+                }).catch(() => {
+                    this.setState({
+                        info : 'red-color',
+                        msg : 'Что-то пошло не так. Скорее всего такое имя уже знято'
+                    })
+                })
+            }
+        }
+    }
+
+    noColor(e) {
+        let styles = this.state.class;
+        styles[e.target.id] = "";
+        this.setState({ class : styles});
+
+        console.log(this.state);
     }
 
     render() {
@@ -31,13 +106,13 @@ class Register extends React.Component {
                                 </p>
                             </div>
                             <div className="form-part form">
-                                <input id="name" type="text" name="itname" placeholder="name" />
-                                <input id="login" type="text" name="ituser" placeholder="login" />
-                                <input id="pw1" type="password" name="itpass1" placeholder="password" />
-                                <input id="pw2" type="password" name="itpass2" placeholder="password again" />
+                                <input id="name" onChange={(e) => this.noColor(e)} className={this.state.class.name} type="text" name="itname" placeholder="name" />
+                                <input id="login" onChange={(e) => this.noColor(e)} className={this.state.class.login} type="text" name="ituser" placeholder="login" />
+                                <input id="pw1" onChange={(e) => this.noColor(e)} className={this.state.class.pw1} type="password" name="itpass1" placeholder="password" />
+                                <input id="pw2" onChange={(e) => this.noColor(e)} className={this.state.class.pw2} type="password" name="itpass2" placeholder="password again" />
 
-                                <div className="reg-info" id="resInfo"></div>
-                                <div className="button submit" id="reg">sign up</div>
+                                <div className={"reg-info " + this.state.info} id="resInfo">{this.state.msg}</div>
+                                <div className="button submit" onClick={() => {this.register()}} id="reg">sign up</div>
                                 <div className="button small" onClick={() => {this.props.toLogin();}}>login page</div>
 
                             </div>
--- application/src/utils/help.js
+++ application/src/utils/help.js
@@ -8,9 +8,34 @@ function getCookie(name) {
 
 function isEmptyOrSpaces(str){
     return str === null || str.match(/^ *$/) !== null;
+}
+
+function validate(document, ids)
+{
+  let data = {};
+  data.errors = [];
+  for(let i = 0; i < ids.length; ++i)
+  {
+    let input = document.getElementById(ids[i]);
+    let val = input.value;
+
+    if(isEmptyOrSpaces(val))
+      data.errors.push(ids[i]);
+    else
+      data[ids[i]] = val;
   }
+
+  return data;
+}
+
+function val(document,id) 
+{
+  return document.getElementById(id).value;
+}
 
 module.exports = {
     getCookie,
-    isEmptyOrSpaces
+    isEmptyOrSpaces,
+    validate,
+    val
 }
