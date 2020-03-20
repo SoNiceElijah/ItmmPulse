67d9ef85a8a34a5bead1c38184130ea9d21631e3
Elijah <Superil2q@yandex.ru>
2020-03-20 21:01:08 +0300

    deploy test

--- lil.js
+++ lil.js
@@ -2,11 +2,34 @@
 
 const express = require('express');
 const router = express.Router();
+const cmd = require('node-cmd');
 
 router.post('/updatePulse', (req,res) => {
     
-    console.log(req.body);
-    res.sendStatus(200);
+
+    if(req.body.ref === 'refs/heads/master')
+    {
+        cmd.get(`
+            git fetch --all
+            git reset --hard origin/master
+            npm i
+            pm2 reload server
+        `,(err,data) => {
+            if(!err)
+            {
+                res.send(data);
+            }
+            else
+            {
+                res.sendStatus(500);
+            }
+
+        })
+    }
+    else
+    {
+        res.send("Update isn't necessary");
+    }
 
 })
 
